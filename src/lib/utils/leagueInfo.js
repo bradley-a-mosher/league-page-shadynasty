04058bc4cf1cd844f813dcb6b61b1e273db27a4a
Brad Mosher <54565328+bradley-a-mosher@users.noreply.github.com>
2024-06-05 13:04:40 -0400

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -97,7 +97,6 @@ export const managers = [
       "bio": "",
       "photo": "/managers/noahTEMP.gif", // square ratio recommended (no larger than 500x500)
       "fantasyStart": 2014, // (optional) when did the manager start playing fantasy football
-      "favoriteTeam": "pit",
       "favoriteTeam": "phi",// (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
       "mode": "Win Now", // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
       "rival": {
